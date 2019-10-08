c56da39dd69c92519e90819dc9b2e2b6d1fc26ed
Wes <wesley.noss@hotmail.com>
2019-10-08 14:53:42 -0500

    Tuesday, 8 October.

--- quiremakersite/quiremaker/views.py
+++ quiremakersite/quiremaker/views.py
@@ -3,11 +3,16 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from django.template import Context, loader
+from django.views.decorators.csrf import csrf_exempt
 
+
+@csrf_exempt
 def index(request):
     template = loader.get_template("quiremaker/index.html")
     return HttpResponse(template.render())
 
+@csrf_exempt
 def submitted(request):
+    dict = request.POST
     template = loader.get_template("quiremaker/submitted.html")
     return HttpResponse(template.render())
